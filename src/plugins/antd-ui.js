527e01f0aae26549dcad8f423de5e4baa7c365d2
Michael van der Haas <kiwi3d@gmail.com>
2021-10-12 15:20:54 +1300

    Update antd-ui.js

--- src/plugins/antd-ui.js
+++ src/plugins/antd-ui.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import {Button, Card, Row, Col} from 'ant-design-vue';
-// import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.css';
 Vue.use(Button, Card, Row, Col)
