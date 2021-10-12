2d943f41da026aeecaaaedf132ef7aac3a51bc2a
Michael van der Haas <kiwi3d@gmail.com>
2021-10-12 19:13:10 +1300

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
       <Cards />
     </div>
     <p>This is represents the next step forward for thl-web projects, utilising a bunch NEW of frameworks</p>
-    <p>- NUXTJS : fast loading between pages using Vue</p>
+    <p>- NUXTJS : A javascript library that treats a website as a single page application.</p>
     <p>- ANTD : a design framework for Vue</p>
     <p>- Keystone 5 integrated</p>
   </div>
