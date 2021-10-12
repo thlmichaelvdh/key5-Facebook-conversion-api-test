e5bdc7b4d4f0ecb79c62eb35bf17ad68d927e0c9
Michael van der Haas <kiwi3d@gmail.com>
2021-10-12 19:45:46 +1300

    fix fix

--- src/pages/car/valiant.vue
+++ src/pages/car/valiant.vue
@@ -40,16 +40,7 @@ export default {
     ],
     script: [
       {
-        src: '  !function(f,b,e,v,n,t,s)\n' +
-            '  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?\n' +
-            '    n.callMethod.apply(n,arguments):n.queue.push(arguments)};\n' +
-            '    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';\n' +
-            '    n.queue=[];t=b.createElement(e);t.async=!0;\n' +
-            '    t.src=v;s=b.getElementsByTagName(e)[0];\n' +
-            '    s.parentNode.insertBefore(t,s)}(window, document,\'script\',\n' +
-            '  \'https://connect.facebook.net/en_US/fbevents.js\');\n' +
-            '  fbq(\'init\', \'140957418204575\');\n' +
-            '  fbq(\'track\', \'PageView\');'
+        src: "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '140957418204575'); fbq('track', 'PageView');"
       }
     ],
     noscript: [{
